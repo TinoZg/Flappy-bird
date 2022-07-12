21cfd34609abce6100a90104be422134f4e20bb3
tinozg <valentino.novakk@gmail.com>
2022-07-12 16:33:41 +0200

    testing CRUD operations on landing page

--- server.js
+++ server.js
@@ -12,49 +12,75 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// The database to use
+// The database name
 const dbName = 'game';
+
+// Database
+let db;
 
 async function run() {
   try {
+    // Start listening on port 3000
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+
+    // Connect to mongo atlas
     await client.connect();
     console.log('Connected to server.');
-    const db = client.db(dbName);
 
+    db = client.db(dbName);
     // Use the collection users
-    const col = db.collection('users');
+    // col = db.collection('users');
 
     // Construct a user
-    let user = {
-      nickname: 'test',
-      score: 10,
-    };
+    // let user = {
+    //   nickname: 'test',
+    //   score: 10,
+    // };
 
-    // Insert signle document, wait for promise so we can read it back
-    const p = await col.insertOne(user);
+    // Insert single document, wait for promise so we can read it back
+    // const p = await col.insertOne(user);
     // Find one document
-    const myDoc = await col.findOne();
+    // const myDoc = await col.findOne();
     // Print to the console
-    console.log(myDoc);
+    // console.log(myDoc);
   } catch (error) {
     console.error(err.stack);
-  } finally {
-    await client.close();
   }
 }
 
-run().catch(console.dir);
-
-// Start listening on port 3000
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+run();
 
 // Serve static files (html, css, front end JavaScript) from public folder
 app.use(express.static('public'));
 
 // Make server parse incoming data as JSON, and limit input size to 1mb
 app.use(express.json({ limit: '1mb' }));
+
+// API path to recieve username and score
+app.post('/test', (request, response) => {
+  if (request.body.score > 0) {
+    const col = db.collection('users');
+    // console.log(request.body);
+    console.log(
+      db
+        .collection('users')
+        .find({})
+        .limit(5)
+        .sort({ score: -1 })
+        .toArray((err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result);
+          }
+        })
+    );
+    // col.insertOne(request.body);
+  }
+  response.end();
+});
 
 // Variable to store username and score
 const userData = {};
